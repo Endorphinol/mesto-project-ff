855046f6b1ad950c78708acde27c30eaffe04af4
Daniel <89999804331@yandex.ru>
2025-02-12 00:38:07 +0300

    1. JS. Первая проверка работы.

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const listCard = document.querySelector('.places__list');
+
+function createCard(Card, deleteCard) {
+    const templateList = document.querySelector('#card-template').content;
+    const templateCard = templateList.querySelector('.card').cloneNode(true);
+    templateCard.querySelector('.card__image').src = Card.link;
+    templateCard.querySelector('.card__title').textContent = Card.name;
+    templateCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return templateCard;
+};
+
+function deleteCard(evt) {
+    const eventClick = evt.target;
+    eventClick.parentElement.remove();
+};
+
+initialCards.forEach(function (item) {
+    const newCard = createCard(item, deleteCard);
+    listCard.append(newCard);
+});
+
+
+
