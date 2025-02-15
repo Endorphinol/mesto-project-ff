5e0251a24d7b80f0c3dbe2b87ec9c87dde9ddc9d
Daniel <89999804331@yandex.ru>
2025-02-15 20:55:56 +0300

    improvement: add const for image

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,9 @@ const listCard = document.querySelector('.places__list');
 function createCard(Card, deleteCard) {
     const templateList = document.querySelector('#card-template').content;
     const templateCard = templateList.querySelector('.card').cloneNode(true);
-    templateCard.querySelector('.card__image').src = Card.link;
-    templateCard.querySelector('.card__image').alt = Card.name;
+    const image = templateCard.querySelector('.card__image');
+    image.src = Card.link;
+    image.alt = Card.name;
     templateCard.querySelector('.card__title').textContent = Card.name;
     templateCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
     return templateCard;
