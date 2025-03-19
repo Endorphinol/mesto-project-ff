6abda08d010c4f2d8b80e717ba9ffe7da7813bac
Daniel <89999804331@yandex.ru>
2025-03-19 19:05:10 +0300

    feat: feat delete card

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,19 @@
-export function createCard(card, deleteCard, addLike, openPopupImage) {
+export function createCard(card, deleteCard, addLike, getUser, openPopupImage) {
     const templateList = document.querySelector('#card-template').content;
     const templateCard = templateList.querySelector('.card').cloneNode(true);
     const image = templateCard.querySelector('.card__image');
+    const likesCount = templateCard.querySelector('.card__count');
+    likesCount.textContent = card.likes.length;
+    console.log(getUser)
+
     templateCard.querySelector('.card__title').textContent = card.name;
     templateCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
     templateCard.querySelector('.card__like-button').addEventListener('click', addLike);
+
+    if (getUser !== card.owner._id) {
+        templateCard.querySelector('.card__delete-button').classList.remove('card__delete-button');
+    }
+
     image.addEventListener('click', () => openPopupImage(card.link, card.name));
     image.src = card.link;
     image.alt = card.name;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import '../pages/index.css';
-import { initialCards } from './cards.js';
 import { createCard, deleteCard, addLike } from './card.js';
 import { openModal, closeModal, closeModalOverlay } from './modal.js';
 import { isValid, showInputError, hideInputError, hasInvalidInput, toggleButtonState, setEventListeners, enableValidation, clearValidation } from './validation.js';
@@ -18,6 +17,7 @@ const popupImage = document.querySelector('.popup__image');
 const popupImageOpen = document.querySelector('.popup_type_image');
 const popupImageDescription = document.querySelector('.popup__caption');
 const cardList = document.querySelector('.places__list');
+
 /**
  * Объект опции
  * 1. Элемент формы.
@@ -35,10 +35,6 @@ const configValidation = {
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible'
 };
-initialCards.forEach(function (item) {
-    const newCard = createCard(item, deleteCard, addLike, openPopupImage);
-    cardList.append(newCard);
-});
 
 buttonAddCard.addEventListener('click', function () {
     clearValidation(formAddCard, configValidation);
@@ -65,6 +61,17 @@ function editProfile(evt) {
     evt.preventDefault();
     nameTitle.textContent = nameInput.value;
     jobTitle.textContent = jobInput.value;
+    fetch('https://nomoreparties.co/v1/wff-cohort-35/users/me', {
+        method: 'PATCH',
+        headers: {
+            authorization: '08a2006d-1e8e-4054-8f6b-d1d1b6dfc2ea',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            about: jobInput.value
+        })
+    });
     closeModal(popupEditProfile);
 };
 
@@ -74,6 +81,17 @@ formAddCard.addEventListener('submit', function (evt) {
         link: document.querySelector('.popup__input_type_url').value,
         name: document.querySelector('.popup__input_type_card-name').value
     };
+    fetch('https://nomoreparties.co/v1/wff-cohort-35/cards', {
+        method: 'POST',
+        headers: {
+            authorization: '08a2006d-1e8e-4054-8f6b-d1d1b6dfc2ea',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: item.name,
+            link: item.link
+        })
+    });
     const newCard = createCard(item, deleteCard, addLike, openPopupImage);
     cardList.prepend(newCard);
     formAddCard.reset();
@@ -90,11 +108,13 @@ function openPopupImage(imageSrc, imageAlt) {
 
 document.addEventListener('click', closeModalOverlay);
 
-
 enableValidation(configValidation);
 
 
-fetch('https://nomoreparties.co/v1/wff-cohort-35/users/me', {
+
+
+
+const getUserData = fetch('https://nomoreparties.co/v1/wff-cohort-35/users/me', {
     method: 'GET',
     headers: {
         authorization: '08a2006d-1e8e-4054-8f6b-d1d1b6dfc2ea'
@@ -103,18 +123,26 @@ fetch('https://nomoreparties.co/v1/wff-cohort-35/users/me', {
     .then(res => res.json())
     .then((result) => {
         document.querySelector('.profile__image').style.backgroundImage = `url(${result.avatar})`;
-        document.querySelector('.profile__title').textContent = result.name;
-        document.querySelector('.profile__description').textContent = result.about;
+        nameTitle.textContent = result.name;
+        jobTitle.textContent = result.about;
+        return result;
     });
 
-
-
-fetch('https://nomoreparties.co/v1/wff-cohort-35/cards', {
+const getCards = fetch('https://nomoreparties.co/v1/wff-cohort-35/cards', {
     method: 'GET',
     headers: {
         authorization: '08a2006d-1e8e-4054-8f6b-d1d1b6dfc2ea'
     }
 })
     .then(res => res.json())
-    .then((result) => {
+
+const promises = [getUserData, getCards]
+
+Promise.all(promises)
+    .then(([userData, cards]) => {
+        const getUser = userData._id;
+        cards.forEach(function (item) {
+            const newCard = createCard(item, deleteCard, addLike, getUser, openPopupImage);
+            cardList.append(newCard);
+        });
     });
